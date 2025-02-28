eab0bf8cfef9b84f2fb95c188f0e02b96b277bd4
kathirviswa <kathirviswa57@gmail.com>
2025-02-28 16:13:01 +0530

    Update QRCode.jsx

--- State-project/src/QRCode.jsx
+++ State-project/src/QRCode.jsx
@@ -7,23 +7,37 @@ const QRCode = () => {
         <label htmlFor="dataInput" className="input-label">
           Enter a text to generate QR Code
         </label>
-        
+
         {/* input content */}
 
         {/* //Label 1 */}
         <label htmlFor="dataInput" className="input-label">
           Data for the QR Code :
         </label>
-        <input type="text" id="dataInput" className="input-field" placeholder="Enter Data for QR Code Number"/>
+        <input
+          type="text"
+          id="dataInput"
+          className="input-field"
+          placeholder="Enter Data for QR Code Number"
+        />
 
         {/* //label 2 */}
         <label htmlFor="dataInput" className="input-label">
           Size of the QR Code (in pixels e.g.,: 150) :
         </label>
-     <input type="text" id="sizeInput" className="input-field" placeholder="Enter Image Size" />
-      
-      <Button type="submit" className="btn">Generate QR Code</Button>
-      <Button type="submit" className="btn">Download QR Code</Button>
+        <input
+          type="text"
+          id="sizeInput"
+          className="input-field"
+          placeholder="Enter Image Size"
+        />
+
+        <Button type="submit" className="btn">
+          Generate QR Code
+        </Button>
+        <Button type="submit" className="btn">
+          Download QR Code
+        </Button>
       </div>
     </div>
   );
