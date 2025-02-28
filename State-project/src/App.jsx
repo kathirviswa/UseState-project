534ed35c3341e22d6d6a3663ebdeb28a4c8f4e66
kathirviswa <kathirviswa57@gmail.com>
2025-02-28 17:33:17 +0530

    h

--- State-project/src/App.jsx
+++ State-project/src/App.jsx
@@ -5,10 +5,10 @@ function App() {
   
 
   return (
-    <>
-    <QrCode/>
-        CD
-    </>
+    
+    <QrCode />
+        
+    
   )
 }
 
--- State-project/src/QRCode.jsx
+++ State-project/src/QRCode.jsx
@@ -1,37 +1,62 @@
-
 const QrCode = () => {
     return (
-      <div className="App-container">
-        <div className="Input">
-        
-         <label htmlFor="dataInput" className="input-label">
+     <div className="App-container">
+      <img src= "./Qrcode.png"/>
+   <div>
+        <label htmlFor="dataInput" className="input-label">
             Enter a text to generate QR Code :
-          </label>
-          <input type="text"
+        </label>
+        <input type="text"
             id="dataInput"
-            className="input-field"
+            // className="input-field"
             placeholder="Enter Data for QR Code Number"
-          />
-     
-          <label htmlFor="dataInput" className="input-label">
-            Size of the QR Code (in pixels e.g.,: 150) :
-          </label>
-          <input type="text"
+        />
+
+   <label htmlFor="sizeInput" className="input-label">
+      Size of the QR Code (in pixels e.g.,: 150) : 
+        </label>
+        <input type="text"
             id="sizeInput"
-            className="input-field"
-            placeholder="Enter Image Size"
-          />
-  
-          <Button type="submit" className="btn">
-            Generate QR Code
-          </Button>
-          <Button type="submit" className="btn">
-            Download QR Code
-          </Button>
-        </div>
-      </div>
+            // className="input-field"
+            placeholder="Enter a Image Size"
+        />
+   
+   <button> Generate QR Cod </button>
+   <button> Download QR Code </button>
+
+   </div>
+   </div>
     );
   };
   
   export default QrCode;
   
+
+  // <div className="App-container">
+  // <div className="Input">
+  
+  //  <label htmlFor="dataInput" className="input-label">
+  //     Enter a text to generate QR Code :
+  //   </label>
+  //   <input type="text"
+  //     id="dataInput"
+  //     className="input-field"
+  //     placeholder="Enter Data for QR Code Number"
+  //   />
+
+  //   <label htmlFor="dataInput" className="input-label">
+  //     Size of the QR Code (in pixels e.g.,: 150) :
+  //   </label>
+  //   <input type="text"
+  //     id="sizeInput"
+  //     className="input-field"
+  //     placeholder="Enter Image Size"
+  //   />
+
+  //   <Button type="submit" className="btn">
+  //     Generate QR Code
+  //   </Button>
+  //   <Button type="submit" className="btn">
+  //     Download QR Code
+  //   </Button>
+  // </div>
