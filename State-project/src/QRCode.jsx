e7d6ecca24cc55d8e1cdee13905631e4921b9a3e
kathirviswa <kathirviswa57@gmail.com>
2025-03-03 13:12:20 +0530

    compete the project qrcode generatoe

--- State-project/src/QRCode.jsx
+++ State-project/src/QRCode.jsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 const QrCode = () => {
 
      const [Qrimg, setQrimg] = useState("");
-     const [Loading, SetLoading] = useState(false)//boolean
-     const [qrData, setQrData] = useState("https://pickmydoc.vercel.app/")
-    //  const [qrSize, setQrSize] = useState("150")
+     const [loading, SetLoading] = useState(false);//boolean..
+     //To set the customize OwnLink to the qrdata (Variable) to store input field in (value) Attribute.
+     const [qrData, setQrData] = useState("https://pickmydoc.vercel.app/"); 
+     const [qrSize, setQrSize] = useState("150");
+    
     //  function generateQrCode(){
     // // setQrimg ("public/Scanner.jpg");
        
@@ -14,27 +16,51 @@ const QrCode = () => {
       async function generateQrCode(){
         SetLoading(true);  // setdefault in (setLoading = true)
         
-        try {
-           const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(qrData)}`;
-           setQrimg(url); 
+      try {
+      //  const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(qrData)}`;
+         
+      //updated for qrcode image size change
+      const url = `https://api.qrserver.com/v1/create-qr-code/?size=${qrSize}x${qrSize}&data=${encodeURIComponent(qrData)}`;
+        setQrimg(url); 
         } 
-        catch (error) {
-            console.error("Error generating QR Code, " + error);
-        } finally {
-            SetLoading(false);  // set back to false when done
+      catch (error) {
+         console.error("Error generating QR Code, " + error);
+        } 
+      finally {
+        SetLoading(false); // set back to false when done
         }
       }
 
+// downloadQRCode functionality call
+   function downloadQR() {
+    // console.log("Downloading QR Code");
+    fetch (Qrimg)//To store the Qrimg variable useState
+    .then ((response)=>response.blob()) //blob means filetypes  
+    //blob data to insert into the <a> tag.create a new variables.
+    .then((blob)=>{
+      const link = document.createElement("a");//create a anchortag to Pass..
+      link.href = URL.createObjectURL(blob)//To assign the link to href from blog data. and blogdata will be converted into object format to pass the bolb.
+      link.download ="qrcode.png"; //download qrcode image from server and convert it into PNG format to  
+      document.body.appendChild(link); //append the link to the body. appendChild means its add the anchortag.(link)
+      link.click(); //to click the link to download the image.
+      document.body.removeChild(link);//remove the link from the body. removeChild means its Remove the Anchortag (link)
+    })   
+  }
+    // Noted Functionality downlodeQr : When there is blob data inside an <a> tag, it is converted into an 
+    // object URL. This object URL represents an image (like a QRcode.png). We then trigger an automatic click 
+    // event on the <a> tag to initiate the download (e.g., qrcode.png). As a result, clicking this function
+    // will automatically download the image.
+   
     return (
      <div className="App-container w-full h-screen flex flex-col  items-center justify-center">
       
-      <h1 className=" text-red-500 font-semibold text-[18px] underline p-[105px,0px] "> 
+    <h1 className=" text-red-500 font-semibold text-[18px] underline p-[105px,0px] "> 
     QR Code Generator</h1>
         
-    {Loading && < Loader className=" animate-spin "/> }
+    {loading && <Loader className=" animate-spin "/> }
 
-{/* Having a Qrimage content automatically Render to  generated Qrimage...Noted Empty Usestate */}
-     {Qrimg && <img src= {Qrimg} className="QRimage p-2 shadow-md w-32 flex "/>}
+{/*Having a Qrimage content automatically Render to  generated Qrimage...Noted Empty Usestate */}
+     {Qrimg && <img src= {Qrimg} className="QRimage p-2 shadow-md  flex "/>}
    <div>
         <label htmlFor="dataInput" className="input-label  block mb-2 text-blue-300 text-[15px] font-medium pt-3">
             Enter a text to generate QR Code :
@@ -42,7 +68,7 @@ const QrCode = () => {
         <input type="text"
             id="dataInput"
             value={qrData}
-            onChange={(e) => setQrData(e.target.value)}
+            onChange={(e) => setQrData(e.target.value)} //what we type to (Exp:Link) set the value
             className="input-field p-3 mb-5 w-full text-[15px] border-2 border-blue-400"
             placeholder="Enter Data for QR Code Number"
         />
@@ -52,12 +78,15 @@ const QrCode = () => {
         </label>
         <input type="text"
             id="sizeInput"
-            className="input-field p-3 mb-5 w-full text-[15px] border-2 border-blue-400 "
+            value={qrSize} // to store the size of the QR Code
+            onChange={(e) => setQrSize(e.target.value)} //what we type to (Exp: size) set the value.
+            className="input-field p-3 mb-5 w-full text-[15px] border-2 border-blue-400"
             placeholder="Enter a Image Size"
         />
-   
-   <button onClick= {generateQrCode} className="generate-btn bg-blue-500 hover:bg-blue-400">Generate QR Cod </button>
-   <button className="downlode-btn bg-emerald-600 ml-3 hover:bg-green-400">Download QR Code </button>
+        
+   <button onClick= {generateQrCode} disabled={loading} 
+   className="generate-btn bg-blue-500 hover:bg-blue-400">Generate QR Cod</button>
+   <button onClick={downloadQR}className="downlode-btn bg-emerald-600 ml-3 hover:bg-green-400">Download QR Code </button>
    
    {/* Footer */}
    <p className="footer mt-7.5  font-medium text-center">Designed By 
@@ -67,35 +96,4 @@ const QrCode = () => {
    </div>
     );
   };
-
   export default QrCode;
-  
-
-  // <div className="App-container">
-  // <div className="Input">
-  
-  //  <label htmlFor="dataInput" className="input-label">
-  //     Enter a text to generate QR Code :
-  //   </label>
-  //   <input type="text"
-  //     id="dataInput"
-  //     className="input-field"
-  //     placeholder="Enter Data for QR Code Number"
-  //   />
-
-  //   <label htmlFor="dataInput" className="input-label">
-  //     Size of the QR Code (in pixels e.g.,: 150) :
-  //   </label>
-  //   <input type="text"
-  //     id="sizeInput"
-  //     className="input-field"
-  //     placeholder="Enter Image Size"
-  //   />
-
-  //   <Button type="submit" className="btn">
-  //     Generate QR Code
-  //   </Button>
-  //   <Button type="submit" className="btn">
-  //     Download QR Code
-  //   </Button>
-  //
