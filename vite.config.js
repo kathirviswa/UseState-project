d6871de3b3c47f11478db9b146301e3040d188e5
kathirviswa <kathirviswa57@gmail.com>
2025-03-03 14:30:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-  resolve: {
-    alias: {
-      '@': './src',
-    },
-  },
+
 })
